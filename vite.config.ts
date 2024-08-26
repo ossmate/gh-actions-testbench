e4fbfd31602d9f0a8459b139f36d66bf0753ace0
Mateusz Ossolinski <mateusz.ossolinski@weareams.com>
2024-08-26 16:47:43 +0200

    disable automatic vercel deployments

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react';
 import packageJson from './package.json';
 
 export default defineConfig({
+  base: "/",
+  // build: {
+    // outDir: 'output',
+    // assetsDir: 'assets',
+    // minify: true,
+    // sourcemap: false,
+  // },
   define: {
     __APP_VERSION__: JSON.stringify(packageJson.version),
   },
