140210a0766b68c686d8443893c9b37ff5888679
Mateusz Ossoliński <mateusz.ossolinski@gmail.com>
2025-02-02 17:43:10 +0100

    test: change

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
-        <h1 className="text-xl font-bold text-center">🚀 Hello Rick and Morty - Fan Service</h1>
+        <h1 className="text-xl font-bold text-center">🚀 Hello Rick and Morty - Hello from AWS!</h1>
       </div>
       <Outlet />
 
